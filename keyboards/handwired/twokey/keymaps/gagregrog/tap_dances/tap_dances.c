96cf79c3d8997804fc1fe4d919110c0b7116174e
Gary Gregory Rogers <gagregrog@gmail.com>
2023-03-22 11:39:59 -0700

    feat: double tap to commit selected

--- keyboards/handwired/twokey/keymaps/gagregrog/tap_dances/tap_dances.c
+++ keyboards/handwired/twokey/keymaps/gagregrog/tap_dances/tap_dances.c
@@ -1,6 +1,12 @@
 #include "gagregrog.h"
 #include "tap_dances.h"
 
+void commit_selected(void) {
+  tap_code16(LGUI(KC_K));
+  wait_ms(50);
+  tap_code16(LGUI(LALT(KC_S)));
+}
+
 // - T_ONE
 static td_tap_t one_state = TD_INIT_STATE;
 static td_action_config actions_one[] = {
@@ -36,12 +42,13 @@ void one_end(tap_dance_state_t *state, void *user_data) {
 static td_tap_t two_state = TD_INIT_STATE;
 static td_action_config actions_two[] = {
   ACTION_TAP_DANCE_CONFIG_KEY(KC_SECRET_2),
-  ACTION_TAP_DANCE_CONFIG_KEY(KC_SECRET_3)
+  ACTION_TAP_DANCE_CONFIG_KEY(KC_SECRET_3),
+  ACTION_TAP_DANCE_CONFIG_FN(commit_selected)
 };
 void two_begin(tap_dance_state_t *state, void *user_data) {
   tap_dance_begin(
     actions_two,
-    2,
+    3,
     &two_state,
     state
   );
@@ -50,7 +57,7 @@ void two_begin(tap_dance_state_t *state, void *user_data) {
 void two_end(tap_dance_state_t *state, void *user_data) {
   tap_dance_end(
     actions_two,
-    2,
+    3,
     &two_state,
     state
   );
