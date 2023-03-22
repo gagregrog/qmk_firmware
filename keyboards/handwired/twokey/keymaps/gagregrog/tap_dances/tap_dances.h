0039c3eca09d9a5263eec03b3934b5137a19baa6
Gary Gregory Rogers <gagregrog@gmail.com>
2023-03-22 11:09:53 -0700

    feat: lock os

--- keyboards/handwired/twokey/keymaps/gagregrog/tap_dances/tap_dances.h
+++ keyboards/handwired/twokey/keymaps/gagregrog/tap_dances/tap_dances.h
@@ -1,8 +1,9 @@
 #include "gagregrog.h"
 
 // screenshot to clipboard
-#define SCRN_C    LCTL(SGUI(KC_4))
-#define SCRN_S    SGUI(KC_4)
+#define SCRN_C      LCTL(SGUI(KC_4))
+#define SCRN_S      SGUI(KC_4)
+#define KC_OS_LOCK  LCTL(LGUI(KC_Q))
 
 enum tap_dance_keys {
   T_ONE,
