17a92a40a810f97d53f88d47aa895bd465f46bdf
Gary Gregory Rogers <gagregrog@gmail.com>
2023-07-06 23:41:17 -0700

    feat(twokey): more secret secret

--- keyboards/handwired/twokey/keymaps/gagregrog/tap_dances/tap_dances.c
+++ keyboards/handwired/twokey/keymaps/gagregrog/tap_dances/tap_dances.c
@@ -4,40 +4,48 @@
 // - All Tap Dances
 
 tap_dance_action_t tap_dance_actions[] = {
-  [T_ONE]  = ACTION_TAP_DANCE_GAGREGROG(one_tap_handle),
-  [T_TWO]  = ACTION_TAP_DANCE_GAGREGROG(two_tap_handle),
+  [T_L1_K1]  = ACTION_TAP_DANCE_GAGREGROG(td_handle_L1_K1),
+  [T_L1_K2]  = ACTION_TAP_DANCE_GAGREGROG(td_handle_L1_K2),
+  [T_L2_K2]  = ACTION_TAP_DANCE_GAGREGROG(td_handle_L2_K2),
 };
 
-
-// - T_ONE
-static td_actions_gagregrog_t actions_one[] = {
+// L1_K1
+static td_actions_gagregrog_t actions_L1_K1[] = {
   KEY__TD_ACTION_GAGREGROG(SCRN_C),
   KEY__TD_ACTION_GAGREGROG(SCRN_S),
   NULL__TD_ACTION_GAGREGROG,
   KEY__TD_ACTION_GAGREGROG(KC_OS_LOCK),
   NULL__TD_ACTION_GAGREGROG,
-  KEY__TD_ACTION_GAGREGROG(KC_SECRET_1),
+  LAYER__TD_ACTION_GAGREGROG(L2),
   NULL__TD_ACTION_GAGREGROG,
   NULL__TD_ACTION_GAGREGROG,
   BOOT__TD_ACTION_GAGREGROG,
   BOOT__TD_ACTION_GAGREGROG
 };
-void one_tap_handle(TD_ARGS_GAGREGROG) {
-  HANDLE_TAP_DANCE_GAGREGROG(actions_one);
+void td_handle_L1_K1(TD_ARGS_GAGREGROG) {
+  HANDLE_TAP_DANCE_GAGREGROG(actions_L1_K1);
 }
 
-
-// - T_TWO
+// L1_K2
 void commit_selected(void) {
   tap_code16(LGUI(KC_K));
   wait_ms(50);
   tap_code16(LGUI(LALT(KC_S)));
 }
-static td_actions_gagregrog_t actions_two[] = {
+static td_actions_gagregrog_t actions_L1_K2[] = {
   KEY__TD_ACTION_GAGREGROG(KC_SECRET_2),
   KEY__TD_ACTION_GAGREGROG(KC_SECRET_3),
   FN__TD_ACTION_GAGREGROG(commit_selected),
 };
-void two_tap_handle(TD_ARGS_GAGREGROG) {
-  HANDLE_TAP_DANCE_GAGREGROG(actions_two);
+void td_handle_L1_K2(TD_ARGS_GAGREGROG) {
+  HANDLE_TAP_DANCE_GAGREGROG(actions_L1_K2);
+}
+
+// L2_K2
+static td_actions_gagregrog_t actions_L2_K2[] = {
+  KEY__TD_ACTION_GAGREGROG(KC_SECRET_1),
+};
+
+void td_handle_L2_K2(TD_ARGS_GAGREGROG) {
+  HANDLE_TAP_DANCE_GAGREGROG(actions_L2_K2);
 }
--- keyboards/handwired/twokey/keymaps/gagregrog/tap_dances/tap_dances.h
+++ keyboards/handwired/twokey/keymaps/gagregrog/tap_dances/tap_dances.h
@@ -5,13 +5,16 @@
 #define SCRN_S      SGUI(KC_4)
 #define KC_OS_LOCK  LCTL(LGUI(KC_Q))
 
+void td_handle_L1_K1(TD_ARGS_GAGREGROG);
+void td_handle_L1_K2(TD_ARGS_GAGREGROG);
+void td_handle_L2_K2(TD_ARGS_GAGREGROG);
+
 enum tap_dance_keys {
-  T_ONE,
-  T_TWO,
+  T_L1_K1,
+  T_L1_K2,
+  T_L2_K2,
 };
 
-#define TD_ONE     TD(T_ONE)
-#define TD_TWO     TD(T_TWO)
-
-void one_tap_handle(TD_ARGS_GAGREGROG);
-void two_tap_handle(TD_ARGS_GAGREGROG);
+#define TD_L1_K1 TD(T_L1_K1)
+#define TD_L1_K2 TD(T_L1_K2)
+#define TD_L2_K2 TD(T_L2_K2)
