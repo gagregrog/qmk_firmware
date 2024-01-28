1b3d2727a2864369352b9ccc1de0549b6bcba8f7
Gary Gregory Rogers <gagregrog@gmail.com>
2024-01-27 18:55:01 -0800

    feat: support left/right flashing

---
+++ keyboards/handwired/dactyl_manuform/4x6/elite_c/rules.mk
@@ -0,0 +1,4 @@
+# use for right half (with trackpoint)
+# Bootloader selection
+# elite-c
+BOOTLOADER = atmel-dfu
---
+++ keyboards/handwired/dactyl_manuform/4x6/pro_micro/rules.mk
@@ -0,0 +1,4 @@
+# use for left half (without trackpoint)
+# Bootloader selection
+# Pro Micro
+BOOTLOADER = caterina
