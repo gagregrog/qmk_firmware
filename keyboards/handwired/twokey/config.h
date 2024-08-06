57764a5b397c3f1d9244a70ddf5a53d4b2bf6d71
Gary Gregory Rogers <gagregrog@gmail.com>
2024-08-05 22:59:51 -0700

    chore: label twokey pins

--- keyboards/handwired/twokey/config.h
+++ keyboards/handwired/twokey/config.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MATRIX_ROW_PINS { F4, F6 }
-#define MATRIX_COL_PINS { F5, F7 }
+#define MATRIX_ROW_PINS { F4, F6 } // A3, A1
+#define MATRIX_COL_PINS { F5, F7 } // A2, A0
 
 #define TAPPING_TERM 250
