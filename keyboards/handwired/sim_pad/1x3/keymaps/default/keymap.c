50c6057b8de4e9e7b3690c55b1bb714a258a43ac
Gary Gregory Rogers <gagregrog@gmail.com>
2025-12-24 23:44:15 -0800

    feat: click tap dance

--- keyboards/handwired/sim_pad/1x3/keymaps/default/keymap.c
+++ keyboards/handwired/sim_pad/1x3/keymaps/default/keymap.c
@@ -2,17 +2,19 @@
 
 #define ESC_DURATION 6000
 #define RET_DURATION 6000
+#define BTN_DURATION 6000
 
 enum {
     TD_ESC_SPECIAL,
     TD_RET_SPECIAL,
+    TD_BTN_SPECIAL,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         TD(TD_ESC_SPECIAL),
         TD(TD_RET_SPECIAL),
-        KC_BTN2
+        TD(TD_BTN_SPECIAL)
     )
 };
 
@@ -24,6 +26,10 @@ static bool ret_hold_active = false;
 static bool ret_physically_held = false;
 static uint16_t ret_timer = 0;
 
+static bool btn_hold_active = false;
+static bool btn_physically_held = false;
+static uint16_t btn_timer = 0;
+
 void matrix_scan_kb(void) {
     if (esc_hold_active && timer_elapsed(esc_timer) > ESC_DURATION) {
         unregister_code(KC_ESC);
@@ -33,6 +39,11 @@ void matrix_scan_kb(void) {
     if (ret_hold_active && timer_elapsed(ret_timer) > RET_DURATION) {
         unregister_code(KC_RETN);
         ret_hold_active = false;
+    }
+
+    if (btn_hold_active && timer_elapsed(btn_timer) > BTN_DURATION) {
+        unregister_code(KC_BTN1);
+        btn_hold_active = false;
     }
 }
 
@@ -86,6 +97,31 @@ void ret_tap_dance_reset(tap_dance_state_t *state, void *user_data) {
     }
 }
 
+void btn_tap_dance_finished(tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        if (state->pressed) {
+            // Press-and-hold: behave like a normal mouse button hold
+            register_code(KC_BTN1);
+            btn_physically_held = true;
+        } else {
+            // Single tap
+            tap_code(KC_BTN1);
+        }
+    } else if (state->count == 2) {
+        // Double tap: hold mouse button for BTN_DURATION seconds
+        register_code(KC_BTN1);
+        btn_timer = timer_read();
+        btn_hold_active = true;
+    }
+}
+
+void btn_tap_dance_reset(tap_dance_state_t *state, void *user_data) {
+    if (btn_physically_held) {
+        unregister_code(KC_BTN1);
+        btn_physically_held = false;
+    }
+}
+
 tap_dance_action_t tap_dance_actions[] = {
     [TD_ESC_SPECIAL] = ACTION_TAP_DANCE_FN_ADVANCED(
         NULL,
@@ -96,5 +132,10 @@ tap_dance_action_t tap_dance_actions[] = {
         NULL,
         ret_tap_dance_finished,
         ret_tap_dance_reset
+    ),
+    [TD_BTN_SPECIAL] = ACTION_TAP_DANCE_FN_ADVANCED(
+        NULL,
+        btn_tap_dance_finished,
+        btn_tap_dance_reset
     ),
 };
