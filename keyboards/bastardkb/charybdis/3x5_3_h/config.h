cd22d299308bc738b78ab6230956c6aa225493d6
Gary Gregory Rogers <gagregrog@gmail.com>
2026-02-08 13:37:18 -0800

    fix: remove keyboard level tapping terms in service of timeless config

--- keyboards/bastardkb/charybdis/3x5_3_h/config.h
+++ keyboards/bastardkb/charybdis/3x5_3_h/config.h
@@ -175,8 +175,3 @@
 #define NO_ACTION_ONESHOT
 
 #define TAPPING_TOGGLE 2
-// Home Row Mod settings
-#define TAPPING_TERM 175
-
-// permissive hold - pressing a mod-tap key and pressing/releasing a second key before the mod-tap key is released registers the mod-tap immediately, even if before the tapping term has been reached
-#define PERMISSIVE_HOLD
